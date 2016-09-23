1187587a362bd75052890e1b5c287a6e5d9bb477
Cyril PATRICE <cpatrice@pagesjaunes.fr>
2016-09-23 16:54:52 +0200

    feat(version): snapshot mode
    use the new release start tag (-SNAPSHOT pattern)

--- step.sh
+++ step.sh
@@ -9,11 +9,7 @@ if [ -n "${RELEASE_TAG}"  ]; then
   BITRISE_GIT_BRANCH="feat/create_${BITRISE_GIT_TAG}"
   
 else
- BITRISE_GIT_TAG=$(echo $(git describe --tags --abbrev=0 ) | sed "s/v[0-9\.]*\.[0-9]*[0-9]/&@/g
-  :a
-  {s/0@/1/g;s/1@/2/g;s/2@/3/g;s/3@/4/g;s/4@/5/g;s/5@/6/g;s/6@/7/g;s/7@/8/g;s/8@/9/g;s/9@/@0/g;t a
-  }
-  s/@/1/g")
+ BITRISE_GIT_TAG=$(echo $(git describe --tags --abbrev=0 ) | sed -f tag.sed)
   tag_message="chore(version): bump version"
   BITRISE_GIT_BRANCH="feat/bump_${BITRISE_GIT_TAG}"
   
