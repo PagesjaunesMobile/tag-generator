f5c539831b1b96df7bc241cef77307cf8005e246
Cyril PATRICE <cpatrice@pagesjaunes.fr>
2017-02-03 10:42:18 +0100

    type(branch): the tag needs to be in the destination branch
    unless GitHub API squash is released

--- step.sh
+++ step.sh
@@ -10,34 +10,28 @@ if [ -n "$TAG_SED_RULES" ]; then
 fi
 if [ -n "${RELEASE_TAG}"  ]; then
   BITRISE_GIT_TAG=$RELEASE_TAG
-  tag_message="chore(version): new release"
-  BITRISE_DEST_BRANCH="feat/create_${BITRISE_GIT_TAG}"
-  
+  tag_message="chore(version): new release"  
 else
  BITRISE_GIT_TAG=$(echo $(git describe --tags --abbrev=0 ) | sed -f $TAG_SED_RULES )
-  tag_message="chore(version): bump version"
-  BITRISE_DEST_BRANCH="feat/bump_${BITRISE_GIT_TAG}"
-  
+  tag_message="chore(version): bump version"  
 fi
 
 envman add --key BITRISE_GIT_TAG --value $BITRISE_GIT_TAG
-git checkout -b $BITRISE_DEST_BRANCH
 git commit --allow-empty -m "${tag_message}"
-envman add --key BITRISE_DEST_BRANCH --value $BITRISE_DEST_BRANCH
 
 git tag -a $BITRISE_GIT_TAG -m "${tag_message}"
 if [ -n "${push}" -a "${push}" == "true" -o "${push}" == "false" ]
 then
 
     if [ "${push}" == "true" ]; then
-     git push --follow-tags origin $BITRISE_DEST_BRANCH
+     git push --follow-tags origin $BITRISE_GIT_BRANCH
     fi
     if (( $? )); then
         echo "Failure" >&2
         exit 1
     else
         echo "Success, new tags are :"
-        echo "$(git tag)"
+        echo "$(git log --tags --simplify-by-decoration --pretty="format:%ci %d" | head -5)"
         # code block that is dependent on success of previous command
     fi
     exit 0
